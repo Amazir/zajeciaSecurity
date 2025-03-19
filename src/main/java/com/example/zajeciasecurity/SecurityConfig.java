47b8d2d72d7a281c5ecbec2c5bc8a6ffafe55cb5
mromasze <m.romaszewski@hotmail.com>
2025-03-19 09:19:45 +0100

    v2

--- src/main/java/com/example/zajeciasecurity/SecurityConfig.java
+++ src/main/java/com/example/zajeciasecurity/SecurityConfig.java
@@ -1,6 +1,8 @@
 package com.example.zajeciasecurity;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -8,6 +10,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 public class SecurityConfig {
@@ -22,5 +25,16 @@ public class SecurityConfig {
                 .build();
 
         return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth.requestMatchers("/secured").authenticated().anyRequest().permitAll())
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults())
+                .logout(logout -> logout.logoutSuccessUrl("/"));
+
+        return http.build();
     }
 }
